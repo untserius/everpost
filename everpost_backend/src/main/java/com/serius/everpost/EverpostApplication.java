444d965515307510729e06770e947f0767e88c0c
Sudhir Das <sudhirdas1999@gmail.com>
2024-04-11 13:20:51 +0530

    initial commit

---
+++ everpost_backend/src/main/java/com/serius/everpost/EverpostApplication.java
@@ -0,0 +1,13 @@
+package com.serius.everpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverpostApplication.class, args);
+	}
+
+}
---
+++ everpost_backend/src/test/java/com/serius/everpost/EverpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serius.everpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
